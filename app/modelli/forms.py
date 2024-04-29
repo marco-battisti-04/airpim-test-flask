e5c3bcc0ad474be5ac95a0e0c0c8125e84f74937
marco-battisti-04 <156814368+marco-battisti-04@users.noreply.github.com>
2024-04-29 15:39:54 +0000

    rimosse classi inutili, aggiunte classi di test

--- app/modelli/forms.py
+++ app/modelli/forms.py
@@ -1,14 +1,21 @@
 
-from flask_wtf import FlaskForm
+'''from flask_wtf import FlaskForm
 from wtforms import Form, BooleanField, StringField, validators, SubmitField, PasswordField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length'''
 
-class loginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+from wtforms import Form, BooleanField, StringField, validators
+from .models import Persona, Test
 
-class testPostForm(FlaskForm):
-    nome = StringField('nome', [validators.Length(min=2, max=25)])
-    cognome = StringField('cognome', [validators.Length(min=2, max=35)])
-    submit = SubmitField('Submit')
-    
+class testPostForm(Form):
+    nome = StringField('nome', [validators.Length(min=2)])
+
+
+# TODO: vedere se è possibile fare tutto con una sola classe in modo dinamico
+# possibile creazione di un unica classe form e prendere solo i campi voluti
+class newPersonaForm(Form):
+    nome = StringField('nome', [validators.Length(min=2)])
+    cognome = StringField('cognome', [validators.Length(min=2)])
+
+class newTestForm(Form):
+    name = StringField('name', [validators.Length(min=2)])
+    surname = StringField('surname', [validators.Length(min=2)])
