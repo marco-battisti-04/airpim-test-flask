e0ae3b138df1dc044225b362fd8d726ade161bc9
marco-battisti-04 <156814368+marco-battisti-04@users.noreply.github.com>
2024-04-29 10:45:52 +0000

    modifiche ai modelli, modificata pagina

---
+++ modelli/models.py
@@ -0,0 +1,13 @@
+
+class Persona:
+
+    def __init__(self, nome, cognome):
+        self.nome = nome
+        self.cognome = cognome
+
+
+class Test:
+
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
--- run.py
+++ run.py
@@ -1,7 +1,41 @@
-from flask import Flask
+# Flask
+# import os
+from flask import Flask, render_template, jsonify
+
+# Models
+
+# FIXME: 1) risolvere problema del modello non trovato
+# from modelli.models import Persona 
 
 app = Flask(__name__)
 
+# route di default
 @app.route("/")
 def default():
-    return "<p>Hello, World!</p>"
+    # la cartella di default per i file è templates
+    people = [Persona("John", "Doe"), Persona("Jane", "Doe"), Persona("Riccardo", "Tognetti"), Persona("Leonardo", "Brugnara")]
+    cols = ["nome", "cognome"]
+    # attribute_names = [attr for attr in dir(Persona("", "")) if not attr.startswith("__")]
+    
+    return render_template("home.html", people=people, cols=cols)
+
+@app.route("/api/test")
+def getTest():
+
+    pass
+    # Convert the dictionary to JSON using jsonify
+
+#FIXME: 2) spostare le righe seguenti in models.py appena risolto il fixme precedente
+
+class Persona:
+
+    def __init__(self, nome, cognome):
+        self.nome = nome
+        self.cognome = cognome
+
+
+class Test:
+
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
