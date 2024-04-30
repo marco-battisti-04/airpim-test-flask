a71dfd027cb25ada75ad05fa03b171ba9377babc
marco-battisti-04 <156814368+marco-battisti-04@users.noreply.github.com>
2024-04-30 09:14:16 +0000

    aggiunti campi fissi ai modelli

--- app/modelli/models.py
+++ app/modelli/models.py
@@ -1,12 +1,16 @@
 
 class Persona:
-
+    nome = ""
+    cognome = ""
+    
     def __init__(self, nome, cognome):
         self.nome = nome
         self.cognome = cognome
 
 
 class Test:
+    name = ""
+    surname = ""
 
     def __init__(self, name, surname):
         self.name = name
